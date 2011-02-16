1561f3c470415f3fe9ca1154360e6e130069c23a
Tetsuya Morimoto <96569+t2y@users.noreply.github.com>
2011-02-16 09:45:43 +0900

    added the detect method for Google Translator refactored for calling Google Translator api

--- src/ikazuchi/izuchi/translator/google.py
+++ src/ikazuchi/izuchi/translator/google.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+import socket
 import urllib2
 from contextlib import closing, nested
 from urllib import urlencode
@@ -17,25 +18,68 @@ class GoogleTranslator(object):
     Google Translate API Terms of Use
     http://code.google.com/intl/ja/apis/language/translate/terms.html
     """
-    def __init__(self, lang_from, lang_to, handler):
-        self.handler = handler
-        self.query = {
-            "v": "1.0",
-            "langpair": "{0}|{1}".format(lang_from, lang_to),
-        }
-        domain = "ajax.googleapis.com"
-        path = "/ajax/services/language/translate?"
-        self._url = "http://{0}{1}".format(domain, path)
+    domain = "ajax.googleapis.com"
+    common_path = "/ajax/services/language/"
+    key = "ABQIAAAAK6kpHnylgmAYtO7ZX01XXRSvW2ISZ2KI4wU-F"\
+          "k6WlRk77d73EhTtYeI1LUl3BfkKv-17KKEWzdRTMw"
+    userip = socket.gethostbyname(socket.gethostname())
 
-    def translate(self, text):
-        translated = ""
-        self.query.update(q=text.encode("utf-8"))
-        url = "{0}{1}".format(self._url, urlencode(self.query))
+    def __init__(self, lang_from, lang_to, handler):
+        self.lang_from = lang_from
+        self.lang_to = lang_to
+        self.handler = handler
+        self._url = "http://{0}{1}".format(self.domain, self.common_path)
+
+    def get_api_path(self, func):
+        return "{0}?".format(func.func_name)
+
+    def get_url(self, func, query):
+        return "{0}{1}{2}".format(self._url,
+                self.get_api_path(func), urlencode(query))
+
+    def request(self, url):
         req = urllib2.Request(url)
         with closing(urllib2.urlopen(req)) as res:
             res_json = json.loads(res.read())
             if res_json["responseStatus"] == 200:
-                translated = res_json["responseData"]["translatedText"]
+                response = res_json["responseData"]
             else:
                 raise RuntimeError(res_json)
-        yield self.api(), translated
+        return response
+
+    def call_api(self, func, query, key=None):
+        """high-level method to make url and request"""
+        url = self.get_url(func, query)
+        response = self.request(url)
+        if key:
+            response = response[key]
+        return self.api(), response
+
+    def detect(self, text):
+        """ JSON Developer's Guide for Language Detect
+        http://code.google.com/intl/ja/apis/language/translate/v1/\
+        using_rest_langdetect.html
+        """
+        query = {
+            "v": "1.0",
+            "q": text.encode("utf-8"),
+            "key": self.key,
+            "userip": self.userip,
+        }
+        api, response = self.call_api(self.detect, query)
+        return self.api(), response
+
+    def translate(self, text):
+        """ JSON Developer's Guide for Translate
+        http://code.google.com/intl/ja/apis/language/translate/v1/\
+        using_rest_translate.html
+        """
+        query = {
+            "v": "1.0",
+            "q": text.encode("utf-8"),
+            "langpair": "{0}|{1}".format(self.lang_from, self.lang_to),
+            "key": self.key,
+            "userip": self.userip,
+        }
+        api, response = self.call_api(self.translate, query, "translatedText")
+        yield api, response
---
+++ src/ikazuchi/tests/test_google_translator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from nose.tools import *
+
+# functions for test
+from ikazuchi.izuchi.translator import TranslatingGoogle
+
+class TestGoogleTranslator(object):
+
+    def setup(self):
+        self.t = TranslatingGoogle("ja", "en", None)
+
+    def test_get_api_path(self):
+        def detect(): pass
+
+        assert_equal("detect?", self.t.get_api_path(detect))
+
+    def test_detect(self):
+        res = list(self.t.detect(unicode("テスト", "utf-8")))[1]
+        assert_equal((False, u"ja"), (res["isReliable"], res["language"]))
+
+    def test_translate(self):
+        assert_equal([('Google', u'Test')],
+                     list(self.t.translate(unicode("テスト", "utf-8"))))
