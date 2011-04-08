a1e494dec05f02fe43e74f754ad2f62b44715882
Tetsuya Morimoto <96569+t2y@users.noreply.github.com>
2011-04-08 23:06:43 +0900

    changed the test more clearly

--- src/ikazuchi/tests/test_translator.py
+++ src/ikazuchi/tests/test_translator.py
@@ -6,8 +6,9 @@ from nose.tools import *
 from ikazuchi.core.translator.utils import *
 
 def test_call_api_with_multithread():
-    def dummy_func(_):
-        return True
-    lines_num = 30
-    actual = call_api_with_multithread(dummy_func, range(lines_num))
-    assert_equal([True] * lines_num, actual)
+    def dummy_func(num):
+        sleep(0.1)
+        return num
+    from time import sleep
+    args = range(30)
+    assert_equal(args, call_api_with_multithread(dummy_func, args))
