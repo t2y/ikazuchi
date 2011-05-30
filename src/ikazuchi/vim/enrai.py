be239e42ce6ae36179be799d6e919fe01926093d
Tetsuya Morimoto <96569+t2y@users.noreply.github.com>
2011-05-30 22:18:51 +0900

    changed to set apikey and api_method for each API appropriately

--- src/ikazuchi/vim/enrai.py
+++ src/ikazuchi/vim/enrai.py
@@ -7,7 +7,8 @@ except ImportError:
 
 import sys
 from ikazuchi.core.translator import TRANSLATE_API
-from utils import (to_unicode, to_encode, get_vim_variables)
+from utils import (to_unicode, to_encode, get_vim_variables,
+                   get_apikey, get_translate_method)
 
 def get_word_on_cursor(enc):
     words = to_unicode(vim.current.line.split(" "), enc)
@@ -20,16 +21,18 @@ def get_word_on_cursor(enc):
             return word
     return None
 
-def translate_with_word(t, enc):
+def translate_with_word(api_method, enc):
     word = get_word_on_cursor(enc)
     if word:
-        api, trans = t.translate([word])
+        api, trans = api_method([word])
         vim.command("let enrai_target_word='{0}'".format(word.encode(enc)))
         print "Translated by {0}: {1}".format(api, trans[0].encode(enc))
 
 def translate(api_name, lang_from, lang_to, enc):
     t = TRANSLATE_API[api_name](lang_from, lang_to, None)
-    return translate_with_word(t, enc)
+    t.set_apikey(get_apikey(api_name))
+    api_method = get_translate_method(t, api_name)
+    return translate_with_word(api_method, enc)
 
 def main():
     try:
--- src/ikazuchi/vim/utils.py
+++ src/ikazuchi/vim/utils.py
@@ -5,6 +5,10 @@ try:
 except ImportError:
     pass
 
+# OVERWRITE THIS VARIABLES WITH YOUR APIKEY
+GOOGLE_APIKEY = "AIzaSyDDCHHwbfHLIsHWEhxAu41UmrRCg_Xmvm8"
+MICROSOFT_APIKEY = "D9D0E326A70EA4E66218F43130890052808A0142"
+
 def to_unicode(seq, enc):
     return [unicode(i, enc) for i in seq]
 
@@ -23,3 +27,19 @@ def get_vim_variables():
         print "Set enc, raimei_api, raimei_from, raimei_to variables!"
         raise
     return api_name, lang_from, lang_to, enc
+
+def get_apikey(api_name):
+    apikey = ""
+    if api_name == "google":
+        apikey = GOOGLE_APIKEY
+    elif api_name == "microsoft":
+        apikey = MICROSOFT_APIKEY
+    return apikey
+
+def get_translate_method(translater, api_name):
+    api_method = lambda api, texts: (api, texts)
+    if api_name == "google":
+        api_method = translater.translate
+    elif api_name == "microsoft":
+        api_method = translater.translate_array
+    return api_method
